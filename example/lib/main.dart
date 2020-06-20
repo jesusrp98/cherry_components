aae2d27b9fe997eb35f9ebfee6e1957c23be7002
jesus <jesusrope98@outlook.com>
2020-06-20 19:52:03 +0200

    Added basic example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,1 +1,48 @@
-// TODO
+import 'package:cherry_components/cherry_components.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'search_page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cherry Components'),
+      ),
+      body: Column(
+        children: [
+          FlatButton(
+            child: Text('BottomRoundDialog'),
+            onPressed: () => showBottomRoundDialog(
+              context: context,
+              title: 'Sample Title',
+              children: [Text('Hello!')],
+            ),
+          ),
+          FlatButton(
+            child: Text('RoundDialog'),
+            onPressed: () => showRoundDialog(
+              context: context,
+              title: 'Sample Title',
+              children: [Text('Hello!')],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
